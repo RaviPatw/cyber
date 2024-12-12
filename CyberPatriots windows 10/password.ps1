b8b2da46351a562052b9e7c701104f48604f0a52
RaviPatw <155745992+RaviPatw@users.noreply.github.com>
2024-12-12 17:30:33 -0500

    Update password.ps1

--- CyberPatriots windows 10/password.ps1
+++ CyberPatriots windows 10/password.ps1
@@ -6,7 +6,7 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 
 # Log file to record actions
 $logFile = "C:\SetUserPasswordsLog.txt"
-Write-Output "Setting all user passwords to P@s$w0r3!" | Out-File -Append $logFile
+Write-Output "Setting all user passwords to P@s$w0r3! and disabling 'Password Never Expires'" | Out-File -Append $logFile
 
 # New password
 $newPassword = "P@s$w0r3!"
@@ -14,14 +14,18 @@ $newPassword = "P@s$w0r3!"
 # Get all local users
 $users = Get-LocalUser | Where-Object { $_.Enabled -eq $true -and $_.Name -ne "Administrator" }
 
-# Iterate through each user and set the password
+# Iterate through each user
 foreach ($user in $users) {
     try {
         # Set the new password
         $user | Set-LocalUser -Password (ConvertTo-SecureString -String $newPassword -AsPlainText -Force)
         Write-Output "Password for user '$($user.Name)' set successfully." | Out-File -Append $logFile
+
+        # Disable "Password Never Expires"
+        $user | Set-LocalUser -PasswordNeverExpires $false
+        Write-Output "Disabled 'Password Never Expires' for user '$($user.Name)'." | Out-File -Append $logFile
     } catch {
-        Write-Output "Failed to set password for user '$($user.Name)': $_" | Out-File -Append $logFile
+        Write-Output "Failed to update user '$($user.Name)': $_" | Out-File -Append $logFile
     }
 }
 
